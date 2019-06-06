c69944b6e1a4a8ba224351c106889a6c16a97791
tca88 <trishnaamin@gmail.com>
2019-06-06 18:56:44 -0400

    Add minor material UI styling to TenantDashboard.js.

--- rentme/src/components/tenant/TenantDashboard.js
+++ rentme/src/components/tenant/TenantDashboard.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
 
 const TenantDashboard = props => {
   return (
     <>
-      <Link to="/view-receipts">Rent Receipts</Link>
-      <Link to="/issue-report">Report an issue</Link>
+      <List>
+        <ListItemLink href="/view-receipts">
+          <ListItemText primary="Rent Receipts" />
+        </ListItemLink>
+      </List>
+      <List>
+        <ListItemLink href="/issue-report">
+          <ListItemText primary="Report an issue" />
+        </ListItemLink>
+      </List>
     </>
-  )
+  );
 };
 
-export default TenantDashboard
+export default TenantDashboard;
