4baf0634bc17a4ecd2090dcfed9d95e0ec4a0e51
cori-peck <cori.peck@ymail.com>
2019-06-06 10:28:19 -0500

    more boilerplating, adds Login, Manager Login, Manager Dash components

---
+++ rentme/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = props => {
+    return (
+        <>
+
+            <button>Login as a Renter</button>
+            <button>Login as a Property Owner</button>
+            <Link to="/manager-login">Login as a Manager</Link>
+
+        </>
+    )
+}
+
+export default Login
