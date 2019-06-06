8715bdb9faa423647c3e755e4f259f2bc3d7e8ff
Jeff Lapp <lappjeff20@gmail.com>
2019-06-06 15:51:32 -0400

    Created IssueForm and RentReceipts scaffolding
    Created basic IssueForm and RentReceipt components
    Added exports to index

--- rentme/src/components/tenant/index.js
+++ rentme/src/components/tenant/index.js
@@ -1,3 +1,5 @@
 import TenantDashboard from './TenantDashboard'
+import IssueForm from './IssueForm'
+import RentReceipts from './RentReceipts'
 
-export { TenantDashboard }
+export { TenantDashboard, IssueForm, RentReceipts }
