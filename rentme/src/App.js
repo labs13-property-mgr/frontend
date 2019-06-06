1c2be8f0753bc6f8df0d1118d80ac7b120d28d5b
Jeff Lapp <lappjeff20@gmail.com>
2019-06-06 17:03:01 -0400

    Re-added routes and imports
    Re-implemented routes and imports removed by clearing a merge conflict

--- rentme/src/App.js
+++ rentme/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
-import Login from './components/Login';
-import ManagerLogin from './components/ManagerLogin';
-import ManagerDash from './components/ManagerDash';
+import Login from "./components/Login";
+import ManagerLogin from "./components/ManagerLogin";
+import { ManagerCard, ManagerDash } from "./components/propertyOwner/manager";
+import PropertyOwnerSignup from "./components/PropertyOwnerSignup.js";
+import RenterSignup from "./components/RenterSignup.js";
+import AddManagerForm from "./components/propertyOwner/addManagerForm.js";
 import { TenantDashboard, IssueForm, RentReceipts } from './components/tenant';
 import './App.css';
 
@@ -12,11 +15,15 @@ class App extends Component {
     return (
       <>
       <Route exact path="/" component={Login} />
-      <Route path="/manager-login" component={ManagerLogin} />
-      <Route path="/manager-dash" component={ManagerDash} />
-      <Route path="/tenant-dash" component={TenantDashboard} />
-      <Route path="/issue-report" component={IssueForm} />
-      <Route path="/view-receipts" component={RentReceipts} />
+       <Route path="/manager-login" component={ManagerLogin} />
+       <Route path="/manager-dash" component={ManagerDash} />
+       <Route path="/tenant-dash" component={TenantDashboard} />
+       <Route path="/owner-signup" component={PropertyOwnerSignup} />
+       <Route path="/renter-signup" component={RenterSignup} />
+       <Route path="/add-manager" component={AddManagerForm} />
+       <Route path="/manager-profile" component={ManagerCard} />
+       <Route path="/issue-report" component={IssueForm} />
+       <Route path="/view-receipts" component={RentReceipts} />
       </>
     );
   }
