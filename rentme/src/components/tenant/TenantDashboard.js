121738cb9af9cefd1b0cbd477c7c7bf2f5d8e05d
tca88 <trishnaamin@gmail.com>
2019-06-06 20:34:56 -0400

    Render user data.

--- rentme/src/components/tenant/TenantDashboard.js
+++ rentme/src/components/tenant/TenantDashboard.js
@@ -1,26 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-
-function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
+import axios from "axios";
 
 const TenantDashboard = props => {
+  let [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://rent-me-app.herokuapp.com/api/users")
+      .then(res => {
+        setUser(res.data[0]);
+      })
+      .catch(console.log("Error"));
+  }, []);
+
   return (
     <>
-      <List>
-        <ListItemLink href="/view-receipts">
-          <ListItemText primary="Rent Receipts" />
-        </ListItemLink>
-      </List>
-      <List>
-        <ListItemLink href="/issue-report">
-          <ListItemText primary="Report an issue" />
-        </ListItemLink>
-      </List>
+      <Link to="/view-receipts">Rent Receipts</Link>
+      <Link to="/issue-report">Report an issue</Link>
+      <p>Name: {user && user.First_name + " " + user.Last_name}</p>
+      <p>Role: {user && user.role}</p>
     </>
   );
 };
