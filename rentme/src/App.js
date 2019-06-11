76c1147129d75d0fc65b7b010fc0445b146276ee
tca88 <trishnaamin@gmail.com>
2019-06-10 20:02:00 -0400

    Create the Dashboard layout for the Property Owner, including populating all data available for properties, and linking to add manager form. Add dummy data for list of managers. Create an add TenantForm. Link each manager dummy data name in Property Owner Dashboard to respective manager card page by id. Create routes for property page by id and manager card by id.

--- rentme/src/App.js
+++ rentme/src/App.js
@@ -5,12 +5,18 @@ import Login from "./components/Login";
 import ManagerLogin from "./components/ManagerLogin";
 import { ManagerCard, ManagerDash } from "./components/propertyOwner/manager";
 
-import { PropertyDash, PropertyCard, addManagerForm, VendorAddressBk, TenantAddressBk } from "./components/propertyOwner";
-import {TenantDashboard, RentReceipts, IssueForm} from "./components/tenant";
+import {
+  PropertyDash,
+  PropertyCard,
+  addManagerForm,
+  VendorAddressBk,
+  TenantAddressBk
+} from "./components/propertyOwner";
+import { TenantDashboard, RentReceipts, IssueForm } from "./components/tenant";
 import PropertyOwnerSignup from "./components/PropertyOwnerSignup.js";
 import RenterSignup from "./components/RenterSignup.js";
 import "./App.css";
-
+import AddTenantForm from "./components/propertyOwner/addTenantForm.js";
 
 class App extends Component {
   render() {
@@ -21,15 +27,17 @@ class App extends Component {
         <Route path="/manager-dash" component={ManagerDash} />
         <Route path="/tenant-dash" component={TenantDashboard} />
         <Route path="/property-dash" component={PropertyDash} />
-        <Route path="/property-card" component={PropertyCard} />
+        {/* <Route path="/property-card" component={PropertyCard} /> */}
         <Route path="/vendor-addbook" component={VendorAddressBk} />
         <Route path="/tenant-addbook" component={TenantAddressBk} />
         <Route path="/owner-signup" component={PropertyOwnerSignup} />
         <Route path="/renter-signup" component={RenterSignup} />
         <Route path="/add-manager" component={addManagerForm} />
-        <Route path="/manager-profile" component={ManagerCard} />
+        <Route path="/manager-profile/:id" component={ManagerCard} />
         <Route path="/view-receipts" component={RentReceipts} />
         <Route path="/issue-report" component={IssueForm} />
+        <Route path="/property-card/:id" component={PropertyCard} />
+        <Route path="/add-tenant" component={AddTenantForm} />
       </>
     );
   }
