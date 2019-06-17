ccf46b8b90baf3a985caae22ad905a0bd519416a
cori-peck <cori.peck@ymail.com>
2019-06-16 22:57:37 -0500

    adds session handling-refactors App to manage state of authUser, adds listener - navbar needs refactor

--- rentme/src/App.js
+++ rentme/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
@@ -27,43 +27,69 @@ import AddIssueForm from "./components/tenant/AddIssueForm";
 import "./App.css";
 
 import * as ROUTES from "./constants/routes";
+import { withFirebase } from "./components/Firebase";
 
-const App = () => (
-  <Router>
-    <>
-      <MenuAppBar />
 
-      {/* Authentication/Account Related */}
-      <Route exact path={ROUTES.LOGIN} component={Login} />
-      <Route exact path={ROUTES.RENTER_SIGNUP} component={RenterSignUp} />
-      <Route exact path={ROUTES.OWNER_SIGNUP} component={PropertyOwnerSignup} />
-      <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-      <Route exact path ={ROUTES.PASSWORD_CHANGE} component={PasswordChange} />
-      <Route exact path={ROUTES.ACCOUNT} component={Account} />
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-      {/* Dashboard Views */}
-      <Route exact path={ROUTES.TENANT_DASHBOARD} component={TenantDashboard} />
-      <Route exact path={ROUTES.OWNER_DASHBOARD} component={PropertyDash} />
+    this.state = {
+      authUser: null,
+    }
+  }
 
-      {/* Property Owner Routes */}
-      <Route exact path={ROUTES.PROPERTY_CARD} component={PropertyCard} />
-      <Route exact path={ROUTES.ADD_TENANT} component={AddTenantForm} />
-      <Route exact path={ROUTES.ADD_PROPERTY} component={AddPropertyForm} />
-      <Route exact path={ROUTES.TENANT_ADDRESSBK} component={TenantAddressBk} />
-      <Route exact path={ROUTES.VENDOR_ADDRESSBK} component={VendorAddressBK} />
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null })
+    })
+  }
 
-      {/* Tenant Routes */}
-      <Route exact path={ROUTES.RENT_RECEIPTS} component={RentReceipts} />
-      <Route exact path={ROUTES.ADD_ISSUE_FORM} component={AddIssueForm} />
-      <Route exact path={ROUTES.TENANT_CARD} component={TenantCard} />
+  componentWillUnmount() {
+    this.listener()
+  }
 
-      {/* Hold for Stretch */}
-      <Route path="/manager-login" component={ManagerLogin} />
-      <Route path="/manager-dash" component={ManagerDash} />
-      <Route path="/add-manager" component={addManagerForm} />
-      <Route path="/manager-profile/:id" component={ManagerCard} />
-    </>
-  </Router>
-);
+  render() {
+    return (
+      <Router>
+      <>
+        <MenuAppBar />
+  
+        {/* Authentication/Account Related */}
+        <Route exact path={ROUTES.LOGIN} component={Login} />
+        <Route exact path={ROUTES.RENTER_SIGNUP} component={RenterSignUp} />
+        <Route exact path={ROUTES.OWNER_SIGNUP} component={PropertyOwnerSignup} />
+        <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route exact path ={ROUTES.PASSWORD_CHANGE} component={PasswordChange} />
+        <Route exact path={ROUTES.ACCOUNT} component={Account} />
+  
+        {/* Dashboard Views */}
+        <Route exact path={ROUTES.TENANT_DASHBOARD} component={TenantDashboard} />
+        <Route exact path={ROUTES.OWNER_DASHBOARD} component={PropertyDash} />
+  
+        {/* Property Owner Routes */}
+        <Route exact path={ROUTES.PROPERTY_CARD} component={PropertyCard} />
+        <Route exact path={ROUTES.ADD_TENANT} component={AddTenantForm} />
+        <Route exact path={ROUTES.ADD_PROPERTY} component={AddPropertyForm} />
+        <Route exact path={ROUTES.TENANT_ADDRESSBK} component={TenantAddressBk} />
+        <Route exact path={ROUTES.VENDOR_ADDRESSBK} component={VendorAddressBK} />
+  
+        {/* Tenant Routes */}
+        <Route exact path={ROUTES.RENT_RECEIPTS} component={RentReceipts} />
+        <Route exact path={ROUTES.ADD_ISSUE_FORM} component={AddIssueForm} />
+        <Route exact path={ROUTES.TENANT_CARD} component={TenantCard} />
+  
+        {/* Hold for Stretch */}
+        <Route path="/manager-login" component={ManagerLogin} />
+        <Route path="/manager-dash" component={ManagerDash} />
+        <Route path="/add-manager" component={addManagerForm} />
+        <Route path="/manager-profile/:id" component={ManagerCard} />
+      </>
+    </Router>
+    )
+  }
+}
 
-export default App;
+export default withFirebase(App);
