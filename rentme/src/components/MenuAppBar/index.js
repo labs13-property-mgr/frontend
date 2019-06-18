fe4f4f82566a324381bdbd0341fa419815d0b516
cori-peck <cori.peck@ymail.com>
2019-06-17 23:01:47 -0500

    attempted to add back in styles, errors

--- rentme/src/components/MenuAppBar/index.js
+++ rentme/src/components/MenuAppBar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-/*import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -9,13 +9,13 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import Logo from "../logo.png";
-*/import "./nav.css";
+import "./nav.css";
 
 import SignOut from '../SignOut';
 import * as ROUTES from "../../constants/routes";
 import { AuthUserContext } from "../Session";
 
-/*const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
@@ -27,105 +27,83 @@ import { AuthUserContext } from "../Session";
   }
 }));
 
-export default function MenuAppBar(props) {
-  const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
 
-  function handleChange(event) {
-    setAuth(event.target.checked);
+const MenuAppBar = () => (
+  <>
+    <AuthUserContext.Consumer>
+      {authUser => authUser ? <MenuAppBarAuth /> : <MenuAppBarNonAuth />}
+    </AuthUserContext.Consumer>
+  </>
+)
+
+
+const MenuAppBarAuth = props => {
+  const classes = useStyles();
+
+  function handleClose() {
+    setAnchorEl(null);
   }
 
   function handleMenu(event) {
     setAnchorEl(event.currentTarget);
   }
 
-  function handleClose() {
-    setAnchorEl(null);
-  }
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
+  <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+      <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
             aria-label="Menu"
-          />
+        />
           <div className="header-content">
             <img className="logo" src={Logo} alt="rentme logo" />
-            <Typography variant="h6" className={classes.title}>
-              RentMe
-            </Typography>
-             {auth && ( 
-              <div>
-                <IconButton
-                  aria-label="Account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
+              <Typography variant="h6" >
+                RentMe
+              </Typography>
+                <div>
+                  <IconButton
+                    aria-label="Account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
                 <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right"
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right"
-                  }}
-                  open={open}
-                  onClose={handleClose}
-                >
-                  {!localStorage.getItem("token") ? (
-                    <div>
-                      <MenuItem onClick={handleClose}>Home</MenuItem>
-                    </div>
-                  ) : (
-                    <div>
-                      <MenuItem onClick={handleClose}>My account</MenuItem>
-                      <MenuItem><SignOut /></MenuItem>
-                    </div>
-                  )}
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right"
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right"
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                  <div>
+                    <Link to={ROUTES.ACCOUNT}><MenuItem>Account</MenuItem></Link>
+                    <MenuItem><SignOut /></MenuItem>
+                  </div>
                 </Menu>
               </div>
-             )}
           </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+      </Toolbar>
+    </AppBar>
+  </div>
 }
-*/
 
-const MenuAppBar = () => (
-<><AuthUserContext.Consumer>{authUser => authUser ? <MenuAppBarAuth /> : <MenuAppBarNonAuth />}</AuthUserContext.Consumer></>
-)
-
-const MenuAppBarAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <SignOut />
-    </li>
-  </ul>
-)
 
 const MenuAppBarNonAuth = () => (
   <ul>
     <li>
-      <Link to={ROUTES.LOGIN}>Log In</Link>
+      <Link to={ROUTES.LOGIN}><MenuItem>Log In</MenuItem></Link>
     </li>
   </ul>
 )
