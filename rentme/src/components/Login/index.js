6eefbf6f7e6aafaac7bddd755f979d1943a5a7fe
tca88 <trishnaamin@gmail.com>
2019-06-20 18:42:52 -0400

    Remove login.css import.

--- rentme/src/components/Login/index.js
+++ rentme/src/components/Login/index.js
@@ -19,7 +19,6 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
-import "./login.css";
 import Background from "../img/login-background-image.png";
 import "typeface-roboto";
 
