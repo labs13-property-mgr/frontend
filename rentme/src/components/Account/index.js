5d901166f2205c8cda172321d0a59b05c38daa8c
cori-peck <cori.peck@ymail.com>
2019-06-21 00:36:51 -0500

    adds error messages for social login emails that already exist in the db

--- rentme/src/components/Account/index.js
+++ rentme/src/components/Account/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import PasswordChangeForm from '../PasswordChange';
 import { withAuthorization, AuthUserContext } from "../Session"
 
+
+const LOG_IN_METHODS = [
+    {
+        id: 'password',
+        provider: null,
+    },
+    {
+        id: 'google.com',
+        provider: 'googleProvider',
+    },
+    {
+        id: 'facebook.com',
+        provider: 'facebookProvider',
+    }
+]
+
 const Account = () => (
     <AuthUserContext.Consumer>
         {authUser => (
                 <>
                 <h4>Account: </h4><p>{authUser.email}</p>
                 <PasswordChangeForm />
+                <LoginManagement />
             </>
         )}
     </AuthUserContext.Consumer>
