1869559c410c782faaa62cc08da06c225dd07c6d
Jeff Lapp <lappjeff20@gmail.com>
2019-06-24 17:14:12 -0400

    Created and imported basic ServiceRequestModal

--- rentme/src/components/propertyOwner/PropertyCard/ServiceRequest.js
+++ rentme/src/components/propertyOwner/PropertyCard/ServiceRequest.js
@@ -1,7 +1,8 @@
 import React, {useState } from "react"
-
+import ServiceRequestModal from './ServiceRequestModal'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
---
+++ rentme/src/components/propertyOwner/PropertyCard/ServiceRequestModal.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react"
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+
+const ServiceRequestModal = () => {
+  const [open, setOpen] = useState(false)
+  
+  return (
+     null
+  )
+}
+
+ export default ServiceRequestModal
