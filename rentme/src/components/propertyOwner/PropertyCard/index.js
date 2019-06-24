9e2cbc8326563526dc5a7abe710b1eb3694010f5
BrandonSaravia <BPSaravia@gmail.com>
2019-06-24 17:13:46 -0400

    adding a date to file sent back to firebase

--- rentme/src/components/propertyOwner/PropertyCard/index.js
+++ rentme/src/components/propertyOwner/PropertyCard/index.js
@@ -9,6 +9,8 @@ class PropertyCard extends Component {
     selectedFile : null
   }
 
+
+  
   handleImageChange = (e) => {
     this.setState({
       selectedFile : e.target.files[0]
@@ -16,9 +18,15 @@ class PropertyCard extends Component {
   }
 
   handleUploadPicture = () => {
+
     const fd = new FormData();
-    fd.append('image', this.state.selectedFile, this.state.selectedFile.name)
+
+    let fullFileName =  Date.now() + this.state.selectedFile.name;
+
+    fd.append('image', this.state.selectedFile, fullFileName)
+
     axios.post('https://us-central1-rentme-52af4.cloudfunctions.net/uploadFile', fd, {
+
       onUploadProgress: progressEvent => {
         console.log('Upload Progress: ' + Math.round(progressEvent.loaded / progressEvent.total * 100) + "%")
       }
@@ -27,6 +35,9 @@ class PropertyCard extends Component {
 
 
   render() {
+
+    // document.getElementById('propertyImage').src = "https://firebasestorage.googleapis.com/v0/b/rentme-52af4.appspot.com/o/resized-" + fileName + "?alt=media&token=" + imageToken;
+
     return (
       <>
         <h1>Property Card</h1>
@@ -34,10 +45,12 @@ class PropertyCard extends Component {
         <div>
 
           {this.selectedFile === null ? (
-            <img src="./" />
+            <img src="" id="propertyImage"/>
           ) : (
             <img src={this.selectedFile} />
           )}
+
+          <img src='https://storage.googleapis.com/rentme-52af4.appspot.com/resized-1561063140099Austin.jpg?GoogleAccessId=firebase-adminsdk-jqauv%40rentme-52af4.iam.gserviceaccount.com&Expires=16447017600&Signature=eGVA9Pi2lHRMFmjGmtcLM6tyKKejEbLeTWdM14CnGgZ9xV%2Fqrvq980bnG9YxUW%2BVAqLLFmSH69OZJ9m1d5JdfUxsVaJyWwzbVMUNeuy52ppARRFNAvjJsZNpsaS4b2e8RfngvxMT1USr3lBHe77IETYB7Pv%2BwcgGdSPNi0Xbw9Rwmdj5hAgslCw5cBSm1wcokUPxceyepj4yO%2FjWDXRXepHHSeF1b7sD8WFut7FGu1aMisPRnTuxD0jqj2u8X%2FfmJMtwlBVQYTUVbBg%2FZlmo11c%2Bf6cZxMRJy%2Fg1FTMt01gF5%2BRyfU8zJxC9My4B5I7sThj4sBJL4%2FpB3kWuSR8T6A%3D%3D' />
           
           <input type="file" onChange={this.handleImageChange}/>
 
