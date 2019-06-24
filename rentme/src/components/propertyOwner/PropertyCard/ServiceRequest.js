eab1abd9ab1ecb7b11c1d85b56225343e92abbd1
Jeff Lapp <lappjeff20@gmail.com>
2019-06-24 17:05:53 -0400

    Create ServiceRequest.js
    Created ServiceRequest skeleton for individual service requests

---
+++ rentme/src/components/propertyOwner/PropertyCard/ServiceRequest.js
@@ -0,0 +1,30 @@
+import React, {useState } from "react"
+
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: "48%",
+    margin: "10px 5px",
+    textAlign: "center",
+  }
+})
+
+const ServiceRequest = () => {
+
+  const classes = useStyles()
+
+  return (
+    <>
+      <Card className={classes.card}>
+        <CardContent>
+          Request
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+ export default ServiceRequest
