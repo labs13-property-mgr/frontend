4b2c678c1392be3812f314503ec0191f9ec7bcb6
Jeff Lapp <lappjeff20@gmail.com>
2019-06-26 17:08:52 -0400

    Implemented delete function

--- rentme/src/components/tenant/TenantDashboard/DeleteButton.js
+++ rentme/src/components/tenant/TenantDashboard/DeleteButton.js
@@ -2,12 +2,13 @@ import React, {useState, useEffect} from "react"
 import Button from "@material-ui/core/Button";
 
 const DeleteButton = props => {
-  const { resolved_tenant, resolved_owner } = props.request
+  const { resolved_tenant, resolved_owner, id } = props.request
+  const { handleDeleteRequest } = props
 
   return (
     <>
       {resolved_tenant && resolved_owner ?
-        <Button color="primary">
+        <Button color="primary" onClick={() => handleDeleteRequest(id)}>
           <i class="material-icons">
             delete_forever
           </i>
