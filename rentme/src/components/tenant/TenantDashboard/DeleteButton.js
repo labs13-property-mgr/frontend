0d97ed212b5f51fb00a4aad027c39723a4271047
Jeff Lapp <lappjeff20@gmail.com>
2019-06-26 16:35:11 -0400

    Created component button to render a delete icon for resolved service requests

---
+++ rentme/src/components/tenant/TenantDashboard/DeleteButton.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react"
+import Button from "@material-ui/core/Button";
+
+const DeleteButton = props => {
+  const { resolved_tenant, resolved_owner } = props.request
+
+  return (
+    <>
+      {resolved_tenant && resolved_owner ?
+        <Button color="primary">
+          <i class="material-icons">
+            delete_forever
+          </i>
+        </Button>
+        :
+        ""
+      }
+    </>
+  )
+}
+
+ export default DeleteButton
