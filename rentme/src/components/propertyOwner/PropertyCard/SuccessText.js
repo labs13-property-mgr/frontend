54d5a20e7f077aff07ce9a528d6f4f9e14ed02ba
Jeff Lapp <lappjeff20@gmail.com>
2019-06-28 16:26:14 -0400

    Created success message component for successful form update

---
+++ rentme/src/components/propertyOwner/PropertyCard/SuccessText.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from "react"
+
+const SuccessText = props => {
+
+  const { isTriggered } = props
+
+  if(!isTriggered) return null
+  return (
+    <div style={{display:"flex", alignItems:"center", justifyContent: "center"}}>
+      <i class="material-icons">
+      check_circle_outline
+      </i>
+      <span>Request Updated</span>
+    </div>
+  )
+}
+
+ export default SuccessText
