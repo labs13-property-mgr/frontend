7e3da8f127b3fbbb6cbc6ccca5bb91748a799224
tca88 <trishnaamin@gmail.com>
2019-07-11 14:52:00 -0400

    Show inidicators in individual service cards within Property Card page on whether the issue has been viewed, approved by both tenant and owner, or needs approval from the tenant to resolve after the owner has resolved. Show more info on these cards such as the specific status.

--- rentme/src/components/propertyOwner/PropertyCard/ServiceRequest.js
+++ rentme/src/components/propertyOwner/PropertyCard/ServiceRequest.js
@@ -1,31 +1,34 @@
-import React from "react"
-import ServiceRequestModal from './ServiceRequestModal'
+import React from "react";
+import ServiceRequestModal from "./ServiceRequestModal";
 
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   card: {
-    minWidth: "45%",
+    minWidth: "50%",
     margin: "10px 5px",
-    textAlign: "center",
+    textAlign: "center"
   }
-})
+});
 
 const ServiceRequest = props => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <>
       <Card className={classes.card}>
         <CardContent>
-          <ServiceRequestModal request={props.request} handleGetRequests={props.handleGetRequests}/>
+          <ServiceRequestModal
+            request={props.request}
+            handleGetRequests={props.handleGetRequests}
+          />
         </CardContent>
       </Card>
     </>
-  )
-}
+  );
+};
 
- export default ServiceRequest
+export default ServiceRequest;
